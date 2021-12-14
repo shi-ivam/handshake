88e753c82dd828b2fcebe5a4e76b98717e2084b5
shi-ivam <rockyshivammm67@gmail.com>
2021-12-14 21:38:44 +0530

    Port

--- index.js
+++ index.js
@@ -226,7 +226,7 @@ app.get("*",(req,res) => {
     res.sendFile(path.join(__dirname,'html','handshake-client','build','index.html'))
 })
 
-app.listen(process.env.NODE_ENV == "production" ? process.env.PORT : 5000, async () => {
+app.listen(process.env.PORT || 5000, async () => {
 
     await sequelize.authenticate()
     // await sequelize.sync({force:true})
