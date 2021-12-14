8a3acab2c53e4fd692c52a27b099df4441906d68
shi-ivam <rockyshivammm67@gmail.com>
2021-12-14 22:31:52 +0530

    Bug fix SSL

--- models/index.js
+++ models/index.js
@@ -15,7 +15,7 @@ if (env == "production") {
     url:process.env.DATABASE_URL,
     dialect: 'postgres',
     dialectOptions: {
-      ssl: true,
+      // ssl: true,
       rejectUnauthorized: false 
     }
   });
